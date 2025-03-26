7a06003ce7c683a85a0a2061f7b623cf90aeb53b
Pranjal Ramesh Kusnake <146627607+itsmepranjal31@users.noreply.github.com>
2025-03-26 23:01:15 +0530

    Update tlc.v
    added ena logic

--- src/tlc.v
+++ src/tlc.v
@@ -39,7 +39,7 @@ module tt_um_tlc(
         if (!rst_n) begin
             state <= HGRE_FRED;
             counter <= 0;
-        end else begin
+        end else (ena) begin
             state <= next_state;
           counter <= (counter >= 4'd13) ? 0 : counter + 1;
         end
